151a9d1ad4a026a5bd1999dfd01f3c04ed7f54f3
HeisenBerg? <admin@tiktokastudio.com>
2024-12-16 08:12:01 +0800

    Update assets.js

--- assets.js
+++ assets.js
@@ -1,5 +1,26 @@
 logo 
 
+https://huggingface.co/spaces/multimodalart/logo-in-context
+
+
 ico
 
 og
+
+
+remove bg
+https://huggingface.co/spaces/not-lain/background-removal
+
+
+image another style
+
+https://huggingface.co/spaces/black-forest-labs/FLUX.1-Redux-dev
+
+logo in product image
+
+https://huggingface.co/spaces/multimodalart/logo-in-context
+
+
+给人穿芒果衣
+
+https://huggingface.co/spaces/HumanAIGC/OutfitAnyone
